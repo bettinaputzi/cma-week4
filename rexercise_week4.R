e0749913cc83f067dd0807eb1e3a2b4d5d8640c7
Bettina Putzi <putzibet@students.zhaw.ch>
2021-05-27 08:02:33 +0200

    completed task 4

--- rexercise_week4.R
+++ rexercise_week4.R
@@ -7,6 +7,8 @@ wildschwein<-read.delim("wildschwein_BE_2056.csv",header = T, sep = ",")
 str(wildschwein)
 wildschwein$E<-as.integer(wildschwein$E)
 wildschwein$N<-as.integer(wildschwein$N)
+wildschwein$DatetimeUTC<-as_datetime(wildschwein$DatetimeUTC)
+wildschwein$TierName<-as.factor(wildschwein$TierName)
 
 testfun <- function(){}
 testfun()
@@ -45,3 +47,37 @@ wildschwein_filter<-wildschwein%>%
   filter(DatetimeUTC>"2015-04-01" & DatetimeUTC<="2015-04-15")
 
 # Task 3
+wildschwein_filter<-wildschwein_filter%>%
+  mutate(roundedDatetime=round_date(DatetimeUTC, "15 mins"))
+
+
+# Task 4 
+wildschwein_filter_sabi<-wildschwein_filter%>%
+  subset(TierName=="Sabi")
+wildschwein_filter_sabi
+
+wildschwein_filter_rosa<-wildschwein_filter%>%
+  subset(TierName=="Rosa")
+
+wildschwein_filter_ruth<-wildschwein_filter%>%
+  subset(TierName=="Ruth")
+
+
+library(plyr)
+wildschwein_filter_j<-join_all(list(wildschwein_filter_sabi,wildschwein_filter_rosa,wildschwein_filter_ruth), by='roundedDatetime', type='left',match="all")
+wildschwein_filter_j
+?join_all
+
+wildschwein_filter_j<-full_join(wildschwein_filter_sabi,wildschwein_filter_rosa,by="roundedDatetime",suffix=c(".Sabi",".Rosa"))
+wildschwein_filter_j<-full_join(wildschwein_filter_j,wildschwein_filter_ruth,by="roundedDatetime")
+
+wildschwein_filter_j <- wildschwein_filter_j %>%
+  mutate(
+    dist_Sabi_Rosa = sqrt((E.Sabi-E.Rosa)^2+(N.Sabi-N.Rosa)^2),   # distance Sabi to Rosa
+    dist_Sabi_Ruth = sqrt((E.Sabi-E)^2+(N.Sabi-N)^2),
+    dist_Rosa_Ruth = sqrt((E.Rosa-E)^2+(N.Rosa-N)^2),
+    close_Sabi_Rosa= dist_Sabi_Rosa < 100,
+    close_Sabi_Ruth= dist_Sabi_Ruth < 100,
+    close_Rosa_Ruth= dist_Rosa_Ruth < 100
+  )
+
